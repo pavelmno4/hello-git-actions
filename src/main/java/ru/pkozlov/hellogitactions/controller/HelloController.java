1d50f24c12224a97800b9d16cef61483fa1d17ee
pkozlov <pavel.mno4@gmail.com>
2021-08-25 00:35:26 +0300

    add param

--- src/main/java/ru/pkozlov/hellogitactions/controller/HelloController.java
+++ src/main/java/ru/pkozlov/hellogitactions/controller/HelloController.java
@@ -1,13 +1,17 @@
 package ru.pkozlov.hellogitactions.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String greeting() {
+    public String greeting(@RequestParam(required = false) String name) {
+        if (name != null) {
+            return "Hello, " + name + "!!";
+        }
         return "Hello, gitActions!";
     }
 }
